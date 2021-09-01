f9479eeae803674fa7f3daab918fed9fdb9b288a
Zeger Hendrikse <zegerh@yahoo.co.uk>
2021-09-01 19:57:56 +0000

    Initial commit

---
+++ src/main/java/exploringaxon/ExploringAxonApplication.java
@@ -0,0 +1,12 @@
+package exploringaxon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploringAxonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExploringAxonApplication.class, args);
+    }
+}
